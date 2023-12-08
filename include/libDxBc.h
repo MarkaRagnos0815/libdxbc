2fd7807387b6a5b9136c7b7687bfc8476a593c1a
Alexander Wagner <a.wagner.extern@senec.com>
2023-12-08 20:07:01 +0100

    initial commit

---
+++ include/libDxBc.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdint.h>
+
+enum TEX_COMPRESS_FLAGS
+{
+    TEX_COMPRESS_DEFAULT = 0,
+
+    TEX_COMPRESS_RGB_DITHER = 0x10000,
+    // Enables dithering RGB colors for BC1-3 compression
+
+    TEX_COMPRESS_A_DITHER = 0x20000,
+    // Enables dithering alpha for BC1-3 compression
+
+    TEX_COMPRESS_DITHER = 0x30000,
+    // Enables both RGB and alpha dithering for BC1-3 compression
+
+    TEX_COMPRESS_UNIFORM = 0x40000,
+    // Uniform color weighting for BC1-3 compression; by default uses perceptual weighting
+
+    TEX_COMPRESS_BC7_USE_3SUBSETS = 0x80000,
+    // Enables exhaustive search for BC7 compress for mode 0 and 2; by default skips trying these modes
+
+    TEX_COMPRESS_BC7_QUICK = 0x100000,
+    // Minimal modes (usually mode 6) for BC7 compression
+
+    TEX_COMPRESS_SRGB_IN = 0x1000000,
+    TEX_COMPRESS_SRGB_OUT = 0x2000000,
+    TEX_COMPRESS_SRGB = (TEX_COMPRESS_SRGB_IN | TEX_COMPRESS_SRGB_OUT),
+    // if the input format type is IsSRGB(), then SRGB_IN is on by default
+    // if the output format type is IsSRGB(), then SRGB_OUT is on by default
+
+    TEX_COMPRESS_PARALLEL = 0x10000000,
+    // Compress is free to use multithreading to improve performance (by default it does not use multithreading)
+};
+
+struct ColorVec {
+    float r;
+    float g;
+    float b;
+    float a;
+};
+
+extern "C" {
+    typedef void (*BC_DECODE)(ColorVec* pColor, const uint8_t* pBC);
+    typedef void (*BC_ENCODE)(uint8_t* pDXT, const ColorVec* pColor, uint32_t flags);
+
+    void BcDecodeBC1(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC2(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC3(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC4U(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC4S(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC5U(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC5S(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC6HU(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC6HS(ColorVec* pColor, const uint8_t* pBC);
+    void BcDecodeBC7(ColorVec* pColor, const uint8_t* pBC);
+
+    void BcEncodeBC1(uint8_t* pBC, const ColorVec* pColor, float threshold, uint32_t flags);
+    // BC1 requires one additional parameter, so it doesn't match signature of BC_ENCODE above
+
+    void BcEncodeBC2(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+    void BcEncodeBC3(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+    void BcEncodeBC4U(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+    void BcEncodeBC4S(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+    void BcEncodeBC5U(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+    void BcEncodeBC5S(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+    void BcEncodeBC6HU(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+    void BcEncodeBC6HS(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+    void BcEncodeBC7(uint8_t* pBC, const ColorVec* pColor, uint32_t flags);
+}
---
+++ source/api.cpp
@@ -0,0 +1,66 @@
+
+#include "bc.h"
+
+
+void BcDecodeBC1(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC1((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC2(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC2((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC3(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC3((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC4U(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC4U((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC4S(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC4S((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC5U(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC5U((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC5S(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC5S((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC6HU(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC6HU((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC6HS(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC6HS((XMVECTOR*)pColor, pBC);
+}
+void BcDecodeBC7(ColorVec* pColor, const uint8_t* pBC) {
+    DirectX::D3DXDecodeBC7((XMVECTOR*)pColor, pBC);
+}
+
+void BcEncodeBC1(uint8_t* pBC, const ColorVec* pColor, float threshold, uint32_t flags) {
+    DirectX::D3DXEncodeBC1(pBC, (XMVECTOR*)pColor, threshold, flags);
+}
+
+void BcEncodeBC2(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC2(pBC, (XMVECTOR*)pColor, flags);
+}
+void BcEncodeBC3(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC3(pBC, (XMVECTOR*)pColor, flags);
+}
+void BcEncodeBC4U(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC4U(pBC, (XMVECTOR*)pColor, flags);
+}
+void BcEncodeBC4S(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC4S(pBC, (XMVECTOR*)pColor, flags);
+}
+void BcEncodeBC5U(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC5U(pBC, (XMVECTOR*)pColor, flags);
+}
+void BcEncodeBC5S(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC5S(pBC, (XMVECTOR*)pColor, flags);
+}
+void BcEncodeBC6HU(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC6HU(pBC, (XMVECTOR*)pColor, flags);
+}
+void BcEncodeBC6HS(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC6HS(pBC, (XMVECTOR*)pColor, flags);
+}
+void BcEncodeBC7(uint8_t* pBC, const ColorVec* pColor, uint32_t flags) {
+    DirectX::D3DXEncodeBC7(pBC, (XMVECTOR*)pColor, flags);
+}
